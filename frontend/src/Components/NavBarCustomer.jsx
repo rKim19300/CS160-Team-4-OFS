3d3681fde47a325388676e4e71f471b2f9f1d8a4
Sephia Laureencia <hemas.laureencia@sjsu.edu>
2024-03-06 18:08:07 -0800

    create shopping cart section

--- frontend/src/Components/NavBarCustomer.jsx
+++ frontend/src/Components/NavBarCustomer.jsx
@@ -6,7 +6,9 @@ import {
   InputRightElement,
   Button,
   IconButton,
+  useDisclosure,
 } from "@chakra-ui/react";
+import ShoppingCart from "../Pages/ShoppingCart";
 import styles from "./NavBarCustomer.module.css";
 import { Search2Icon } from "@chakra-ui/icons";
 import { FaCartShopping } from "react-icons/fa6";
@@ -22,41 +24,44 @@ export default function NavBarCustomer() {
     setSearchText("");
   };
 
-  const handleCart = () => {
-    console.log("cart is clicked");
-  };
+  const { isOpen, onToggle, onClose } = useDisclosure();
+  const btnRef = React.useRef();
 
   return (
-    <Flex className={styles.container}>
-      <Flex className={styles.leftSide}>
-        <a href="/customer" className={styles.linkWrapper}>
-          <img className={styles.logoImg} src="/logo.png" />
-        </a>
+    <>
+      <Flex className={styles.container}>
+        <Flex className={styles.leftSide}>
+          <a href="/customer" className={styles.linkWrapper}>
+            <img className={styles.logoImg} src="/logo.png" />
+          </a>
 
-        <InputGroup className={styles.searchBarContainer}>
-          <Input
-            value={searchText}
-            onChange={handleChange}
-            type="text"
-            placeholder="Search "
-          />
-          <InputRightElement>
-            <IconButton
-              className={styles.searchIcon}
-              icon={<Search2Icon />}
-              onClick={handleSearch}
+          <InputGroup className={styles.searchBarContainer}>
+            <Input
+              value={searchText}
+              onChange={handleChange}
+              type="text"
+              placeholder="Search "
             />
-          </InputRightElement>
-        </InputGroup>
-      </Flex>
+            <InputRightElement>
+              <IconButton
+                className={styles.searchIcon}
+                icon={<Search2Icon />}
+                onClick={handleSearch}
+              />
+            </InputRightElement>
+          </InputGroup>
+        </Flex>
 
-      <Button
-        className={styles.cartButton}
-        leftIcon={<FaCartShopping />}
-        onClick={handleCart}
-      >
-        Cart
-      </Button>
-    </Flex>
+        <Button
+          className={styles.cartButton}
+          leftIcon={<FaCartShopping />}
+          ref={btnRef}
+          onClick={onToggle}
+        >
+          Cart
+        </Button>
+      </Flex>
+      <ShoppingCart isOpen={isOpen} onClose={onClose} btnRef={btnRef} />
+    </>
   );
 }
