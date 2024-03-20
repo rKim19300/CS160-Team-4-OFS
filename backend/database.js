426e47c9affd622a2eaf15ce8487f230288b3856
Lucas Stevenson <45473315+LucasStevenson@users.noreply.github.com>
2024-03-19 21:43:46 -0700

    fixed duplicate function in database.js

--- backend/database.js
+++ backend/database.js
@@ -119,24 +119,17 @@ class DB {
   // AUTH queries
   ///////
   static async get_user_from_email(email) {
-    let q = await db.query(
-      "SELECT user_id, email, username, user_type FROM Users WHERE email = ?",
-      [email]
-    );
+    let q = await db.query("SELECT user_id, email, username, user_type FROM Users WHERE email = ?", [email]);
     return q[0];
   }
 
   static async insert_new_user(email, username, hashedPw, user_type=UserType.CUSTOMER) {
-      await db.query("INSERT INTO Users(email, username, password, user_type) VALUES (?, ?, ?, ?)", 
-      [email, username, hashedPw, user_type]
-      );
+      await db.query("INSERT INTO Users(email, username, password, user_type) VALUES (?, ?, ?, ?)", [email, username, hashedPw, user_type]);
   }
 
 
   static async get_stored_password(email) {
-    let q = await db.query("SELECT password FROM Users WHERE email = ?", [
-      email,
-    ]);
+    let q = await db.query("SELECT password FROM Users WHERE email = ?", [email]);
     return q[0].password;
   }
   
@@ -144,10 +137,7 @@ class DB {
   // USER queries
   ///////
   static async update_user_info(user_id, username, email) {
-    await db.query(
-      "UPDATE Users SET username = ?, email = ? WHERE user_id = ?",
-      [username, email, user_id]
-    );
+    await db.query("UPDATE Users SET username = ?, email = ? WHERE user_id = ?", [username, email, user_id]);
   }
 
   static async remove_user_by_id(user_id) {
@@ -158,103 +148,57 @@ class DB {
   // PRODUCTS queries
   ///////
   static async select_all_products() {
-      let prods = await db.query("SELECT * FROM Products");
-      return prods;
-  }
-
-
-  ///////
-  // PRODUCTS queries
-  ///////
-  static async select_all_products() {
     let prods = await db.query("SELECT * FROM Products");
     return prods;
   }
 
   static async get_product_info(product_id) {
-    let q = await db.query("SELECT * FROM Products WHERE product_id = ?", [
-      product_id,
-    ]);
+    let q = await db.query("SELECT * FROM Products WHERE product_id = ?", [product_id]);
     if (q.length === 0) {
-      return {
-        productInfo: null,
-        errMsg: `Product with id ${product_id} does not exist`,
-      };
+      return { productInfo: null, errMsg: `Product with id ${product_id} does not exist` };
     }
     return { productInfo: q[0], errMsg: null };
   }
 
-  static async add_new_product(
-    name,
-    description,
-    image_url,
-    price,
-    weight,
-    quantity
-  ) {
-    await db.query(
-      "INSERT INTO Products(name, description, image_url, price, weight, quantity) VALUES (?, ?, ?, ?, ?, ?)",
-      [name, description, image_url, price, weight, quantity]
-    );
+  static async add_new_product(name, description, image_url, price, weight, quantity) {
+    await db.query("INSERT INTO Products(name, description, image_url, price, weight, quantity) VALUES (?, ?, ?, ?, ?, ?)", [name, description, image_url, price, weight, quantity]);
   }
 
   ///////
   // CART queries
   ///////
   static async get_cart_id(user_id) {
-    let q = await db.query("SELECT cart_id FROM Cart WHERE user_id = ?", [
-      user_id,
-    ]);
+    let q = await db.query("SELECT cart_id FROM Cart WHERE user_id = ?", [user_id]);
     if (q.length !== 0) return q[0]["cart_id"];
     await db.query("INSERT INTO Cart(user_id) VALUES (?)", [user_id]);
-    let res = await db.query("SELECT cart_id FROM Cart WHERE user_id = ?", [
-      user_id,
-    ]);
+    let res = await db.query("SELECT cart_id FROM Cart WHERE user_id = ?", [user_id]);
     return res[0]["cart_id"];
   }
 
   static async insert_item_into_cart(cart_id, product_id, quantity) {
-    await db.query(
-      "INSERT INTO Cart_items(cart_id, product_id, quantity) VALUES (?, ?, ?) ON CONFLICT(cart_id, product_id) DO UPDATE SET quantity = quantity + ?",
-      [cart_id, product_id, quantity, quantity]
-    );
+    await db.query("INSERT INTO Cart_items(cart_id, product_id, quantity) VALUES (?, ?, ?) ON CONFLICT(cart_id, product_id) DO UPDATE SET quantity = quantity + ?", [cart_id, product_id, quantity, quantity]);
   }
 
   static async remove_item_from_cart(cart_id, product_id) {
-    await db.query(
-      "DELETE FROM Cart_items WHERE cart_id = ? AND product_id = ?",
-      [cart_id, product_id]
-    );
+    await db.query("DELETE FROM Cart_items WHERE cart_id = ? AND product_id = ?", [cart_id, product_id]);
   }
 
   static async get_cart_items(cart_id) {
-    let q = await db.query(
-      "SELECT p.product_id, p.name, p.description, p.price, p.weight, p.image_url, ci.quantity FROM Cart_items as ci INNER JOIN Products as p ON ci.product_id = p.product_id WHERE ci.cart_id = ?",
-      [cart_id]
-    );
+    let q = await db.query("SELECT p.product_id, p.name, p.description, p.price, p.weight, p.image_url, ci.quantity FROM Cart_items as ci INNER JOIN Products as p ON ci.product_id = p.product_id WHERE ci.cart_id = ?", [cart_id]);
     return q;
   }
 
   static async modify_cart_item_quantity(cart_id, product_id, quantity) {
-    await db.query(
-      "UPDATE Cart_Items SET quantity = ? WHERE cart_id = ? AND product_id = ?",
-      [quantity, cart_id, product_id]
-    );
+    await db.query("UPDATE Cart_Items SET quantity = ? WHERE cart_id = ? AND product_id = ?", [quantity, cart_id, product_id]);
   }
 
   static async get_cart_weight(cart_id) {
-    let q = await db.query(
-      "SELECT SUM(p.weight * ci.quantity) FROM Cart_Items as ci INNER JOIN Products as p ON ci.product_id = p.product_id WHERE ci.cart_id = ?",
-      [cart_id]
-    );
+    let q = await db.query("SELECT SUM(p.weight * ci.quantity) FROM Cart_Items as ci INNER JOIN Products as p ON ci.product_id = p.product_id WHERE ci.cart_id = ?", [cart_id]);
     return q[0]["SUM(p.weight * ci.quantity)"] || 0;
   }
 
   static async get_cart_subtotal_cost(cart_id) {
-    let q = await db.query(
-      "SELECT SUM(p.price * ci.quantity) FROM Cart_Items as ci INNER JOIN Products as p ON ci.product_id = p.product_id WHERE ci.cart_id = ?",
-      [cart_id]
-    );
+    let q = await db.query("SELECT SUM(p.price * ci.quantity) FROM Cart_Items as ci INNER JOIN Products as p ON ci.product_id = p.product_id WHERE ci.cart_id = ?", [cart_id]);
     return q[0]["SUM(p.price * ci.quantity)"] || 0;
   }
 
@@ -263,10 +207,7 @@ class DB {
   }
 
   static async get_cart_item_quantity(cart_id, product_id) {
-    let q = await db.query(
-      "SELECT quantity FROM Cart_items WHERE cart_id = ? AND product_id = ?",
-      [cart_id, product_id]
-    );
+    let q = await db.query("SELECT quantity FROM Cart_items WHERE cart_id = ? AND product_id = ?", [cart_id, product_id]);
     return q.length > 0 ? q[0]["quantity"] : 0;
   }
 
@@ -277,30 +218,13 @@ class DB {
     // TODO: FINISH THIS
   }
 
-  static async add_new_order(
-    user_id,
-    cost,
-    weight,
-    address,
-    delivery_fee,
-    created_at,
-    cart_id
-  ) {
+  static async add_new_order(user_id, cost, weight, address, delivery_fee, created_at, cart_id) {
     // inserts the order data into the `Orders` table and the `Order_items` table
-    await db.query(
-      "INSERT INTO Orders(user_id, cost, weight, address, delivery_fee, created_at) VALUES (?, ?, ?, ?, ?, ?)",
-      [user_id, cost, weight, address, delivery_fee, created_at]
-    );
-    let cart_items = await db.query(
-      "SELECT product_id, quantity FROM Cart_items WHERE cart_id = ?",
-      [cart_id]
-    );
+    await db.query("INSERT INTO Orders(user_id, cost, weight, address, delivery_fee, created_at) VALUES (?, ?, ?, ?, ?, ?)", [user_id, cost, weight, address, delivery_fee, created_at]);
+    let cart_items = await db.query("SELECT product_id, quantity FROM Cart_items WHERE cart_id = ?", [cart_id]);
     for (let cart_item of cart_items) {
       let { product_id, quantity } = cart_item;
-      await db.query(
-        "INSERT INTO Order_items(product_id, quantity) VALUES (?, ?)",
-        [product_id, quantity]
-      );
+      await db.query("INSERT INTO Order_items(product_id, quantity) VALUES (?, ?)", [product_id, quantity]);
     }
   }
 
