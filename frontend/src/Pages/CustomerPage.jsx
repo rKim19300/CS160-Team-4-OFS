f541fc3644f32cbabc5584be1dee5325884951f5
Sephia Laureencia <hemas.laureencia@sjsu.edu>
2024-04-24 02:43:49 -0700

    change carousel to grid

--- frontend/src/Pages/CustomerPage.jsx
+++ frontend/src/Pages/CustomerPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { Flex, Text } from "@chakra-ui/react";
+import { Flex, Text, Grid } from "@chakra-ui/react";
 import { useParams, Outlet, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import styles from "./CustomerPage.module.css";
 import NavBarCustomer from "../Components/NavBarCustomer";
@@ -28,14 +29,16 @@ export default function CustomerPage() {
       if (category_name === "ALL") {
         response = await axiosInstance.get("/api/allProducts");
       } else {
-        response = await axiosInstance.get(`/api/products/category_name=${category_name}`);
+        response = await axiosInstance.get(
+          `/api/products/category_name=${category_name}`
+        );
       }
       if (response.status !== 200) {
         setDisplayedProducts([]);
         return;
       }
       // only display products that are available (quantity > -1)
-      setDisplayedProducts(response.data.filter(prod => prod.quantity > -1));
+      setDisplayedProducts(response.data.filter((prod) => prod.quantity > -1));
       console.log(displayedProducts);
     } catch (err) {
       console.error(err);
@@ -89,7 +92,25 @@ function CustomerPageBody({ category, products }) {
   return (
     <Flex className={styles.bodyContainer}>
       <Text className={styles.categoryText}>{category}</Text>
-      <ProductCarousel products={products} />
+      {/* <ProductCarousel products={products} /> */}
+      <Grid templateColumns="repeat(5, 1fr)" gap="48px" marginTop="16px">
+        {products.map((product, idx) => (
+          <ProductItem key={idx} product={product} />
+        ))}
+      </Grid>
+    </Flex>
+  );
+}
+
+function ProductItem({ product }) {
+  return (
+    <Flex className={styles.productItem}>
+      <img className={styles.prodImg} src={product.image_url} />
+      <Link to={`/customer/productInfo/${product.product_id}`}>
+        <Text className={styles.productTitle}>{product.name}</Text>
+      </Link>
+      <Text className={styles.productPrice}>${product.price.toFixed(2)}</Text>
+      <Text className={styles.productWeight}>{product.weight} lbs</Text>
     </Flex>
   );
 }
